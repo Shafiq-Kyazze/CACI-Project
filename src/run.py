b55270fbceebe86ea2ad071316df4961a8c2d776
Shafiq-Kyazze <shafiqkyazze@live.com>
2021-08-02 11:27:41 +0100

    CHanging name of api folder to api

--- src/run.py
+++
@@ -1,7 +0,0 @@
-"""Run python file to be ran inordered to start the API"""
-from main.init import init_app
-
-app = init_app()
-
-if __name__ == '__main__':   #Making sure API is run  when run.py is ran
-    app.run(host="0.0.0.0")
